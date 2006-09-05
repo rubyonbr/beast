dcc0145b79a395bcc158689ab46d0081440eba15
jgoebel <jgoebel@567b1171-46fb-0310-a4c9-b4bef9110e78>
2006-09-05 14:58:24 +0000

    add block
    git-svn-id: http://svn.techno-weenie.net/projects/beast/trunk@2085 567b1171-46fb-0310-a4c9-b4bef9110e78

--- db/migrate/033_add_password_hash.rb
+++ db/migrate/033_add_password_hash.rb
@@ -5,8 +5,9 @@ class AddPasswordHash < ActiveRecord::Migration
     # you need to edit your environment.rb and pick a non-default password salt
     # before you continue
     raise "ChangePasswordHash" if PASSWORD_SALT == '48e45be7d489cbb0ab582d26e2168621'
-    say_with_time "Hashing all your passwords in 30 seconds... this is a big deal (because it's not reversible), cancel if you aren't ready."
-    sleep 30
+    say_with_time "Hashing all your passwords in 30 seconds... this is a big deal (because it's not reversible), cancel if you aren't ready." do
+      sleep 30
+    end
     rename_column :users, :password, :password_hash
     say_with_time "Hashing passwords..." do
       User.find(:all, :select => 'id, password_hash').each do |u|
