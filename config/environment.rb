bc474495171cf0e217387ee84e62d251aa61a609
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2007-01-08 05:34:48 +0000

    add white_list_formatted_content plugin
    git-svn-id: http://svn.techno-weenie.net/projects/beast/trunk@2639 567b1171-46fb-0310-a4c9-b4bef9110e78

--- config/environment.rb
+++ config/environment.rb
@@ -52,33 +52,6 @@ end
 
 # Include your application configuration below
 PASSWORD_SALT = '48e45be7d489cbb0ab582d26e2168621' unless Object.const_defined?(:PASSWORD_SALT)
-ActiveRecord::Base.class_eval do
-  include ActionView::Helpers::TagHelper, ActionView::Helpers::TextHelper, WhiteListHelper
-  def self.format_attribute(attr_name)
-    class << self; include ActionView::Helpers::TagHelper, ActionView::Helpers::TextHelper, WhiteListHelper; end
-    define_method(:body)       { read_attribute attr_name }
-    define_method(:body_html)  { read_attribute "#{attr_name}_html" }
-    define_method(:body_html=) { |value| write_attribute "#{attr_name}_html", value }
-    before_save :format_content
-  end
-
-  def dom_id
-    [self.class.name.downcase.pluralize.dasherize, id] * '-'
-  end
-
-  protected
-    def format_content
-      body.strip! if body.respond_to?(:strip!)
-      if body.blank?
-        self.body_html = ''
-      else
-        self.body_html = auto_link body do |text|
-          truncate(text, 50)
-        end
-        self.body_html = white_list(RedCloth.new(body_html).to_html)
-      end
-    end
-end
 
 Module.class_eval do
   def expiring_attr_reader(method_name, value)
