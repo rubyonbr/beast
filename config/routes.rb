2855b9f3bccd6944dc108ceab56012b22c904987
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2007-02-27 03:54:37 +0000

    wrong session controller name
    git-svn-id: http://svn.techno-weenie.net/projects/beast/trunk@2766 567b1171-46fb-0310-a4c9-b4bef9110e78

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   map.home '', :controller => 'forums', :action => 'index'
 
-  map.resource :sessions
+  map.resource :session
   
   map.resources :users, :member => { :admin => :post } do |user|
     user.resources :moderators
