92475bd804b47af05d4fad8a3e65c46d5f9ff910
jgoebel <jgoebel@567b1171-46fb-0310-a4c9-b4bef9110e78>
2007-04-20 04:46:26 +0000

    don't try and send e-mails form a domain with a port in the name
    git-svn-id: http://svn.techno-weenie.net/projects/beast/trunk@2845 567b1171-46fb-0310-a4c9-b4bef9110e78

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -4,7 +4,7 @@ class UserMailer < ActionMailer::Base
     @subject    = 'Welcome to Beast'
     @body       = {:user => user, :domain => domain}
     @recipients = user.email
-    @from       = 'beast@'+domain
+    @from       = 'beast@' + domain.split(":").first
     @sent_on    = sent_at
     @headers    = {}
   end
