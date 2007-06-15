c9e9badff662754d64f72ed0b5cbb3d2206c150c
technoweenie <technoweenie@567b1171-46fb-0310-a4c9-b4bef9110e78>
2007-06-15 23:13:33 +0000

    fix posts sweeper [Mike Clark]
    git-svn-id: http://svn.techno-weenie.net/projects/beast/trunk@2911 567b1171-46fb-0310-a4c9-b4bef9110e78

--- app/models/posts_sweeper.rb
+++ app/models/posts_sweeper.rb
@@ -2,7 +2,8 @@ class PostsSweeper < ActionController::Caching::Sweeper
   observe Post
   
   def after_save(post)
-    FileUtils.rm_rf File.join(RAILS_ROOT, 'public', 'forums', post.forum_id.to_s, 'topics', post.topic_id.to_s)
+    FileUtils.rm_rf File.join(RAILS_ROOT, 'public', 'forums', post.forum_id.to_s, 'posts.rss')
+    FileUtils.rm_rf File.join(RAILS_ROOT, 'public', 'forums', post.forum_id.to_s, 'topics', "#{post.topic_id}.rss")
     FileUtils.rm_rf File.join(RAILS_ROOT, 'public', 'users')
     FileUtils.rm_rf File.join(RAILS_ROOT, 'public', 'posts.rss')
   end
